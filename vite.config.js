f5dd85bc391cd115f8fc5a8f4a41ec8cb5be2801
Vladyslav <vladzvezdaev@gmail.com>
2025-12-30 13:04:19 +0200

    fix(deploy): configure vite base path and build project

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,18 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  // Настройки сервера для автоматического обновления
+  base: "/first-pro-landing/",
+
   server: {
     watch: {
-      // Использовать опрос файлов (помогает, если обычный HMR не срабатывает)
       usePolling: true,
     },
-    // Открывать браузер автоматически при старте (по желанию)
+
     open: true,
   },
   css: {
     preprocessorOptions: {
-      scss: {
-        // additionalData: `@import "@/styles/_variables.scss";`
-      },
+      scss: {},
     },
   },
   resolve: {
